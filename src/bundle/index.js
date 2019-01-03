314ef023080beac39399d3df5c4874b8d750cc09
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-01-03 11:10:59 +0100

    Rewritten rewritters

--- src/bundle/index.js
+++ src/bundle/index.js
@@ -1,18 +1,18 @@
 'use strict'
 
 const { emptyDir, outputFile } = require('fs-extra')
+const { each, eachLimit } = require('aigle')
 const timeSpan = require('time-span')
 const getHTML = require('html-get')
 const { size } = require('lodash')
 const { URL } = require('url')
-const aigle = require('aigle')
 
 const { promisify } = require('util')
 
 const countFiles = promisify(require('count-files'))
 
 const download = require('./download')
-const extract = require('./extract')
+const rewrite = require('./rewrite')
 
 const RE_LAST_TRAILING_SLASH = /\/$/
 
@@ -22,29 +22,32 @@ const getFileName = ({ pathname }) => {
   return `${filename}.html`
 }
 
-const bundle = async (url, { total, cache, emitter, output, prerender }) => {
-  const { html: originalHtml } = await getHTML(url, { prerender })
-  const { urls, html, rewrite } = await extract({ html: originalHtml, url })
-  const downloader = download({ cache, output, emitter, rewrite })
+const bundleUrl = async (url, { total, cache, emitter, output, prerender }) => {
+  const { html: rawHtml } = await getHTML(url, { prerender })
+  const { vinylUrls, html } = await rewrite({ html: rawHtml, url })
+
+  const downloader = download({ cache, output, emitter })
   const filename = getFileName(new URL(url))
   let time = timeSpan()
-  await aigle.each(urls, downloader)
+  await each(vinylUrls, downloader)
   await outputFile(`${output}/${filename}`, html)
   time = time()
   emitter.emit('url', { url, filename, time, total })
 }
 
-module.exports = async (
+const bundle = async (
   urls,
   { concurrence, emitter, cache, output, ...opts }
 ) => {
   await emptyDir(output)
   const total = size(urls)
   let time = timeSpan()
-  await aigle.eachLimit(urls, concurrence, url =>
-    bundle(url, { total, cache, emitter, output, ...opts })
+  await eachLimit(urls, concurrence, url =>
+    bundleUrl(url, { total, cache, emitter, output, ...opts })
   )
   time = time()
   const { files, bytes } = await countFiles(output)
   emitter.emit('end', { urls: Array.from(cache), files, bytes, time })
 }
+
+module.exports = bundle
